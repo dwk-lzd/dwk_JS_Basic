ca12f388cf3377084e612718af11195f277db044
dwk-lzd <2920399114@qq.com>
2025-06-23 23:47:18 +0800

    删除图书功能

--- AJAX入门/2day/2day案例/js/index.js
+++ AJAX入门/2day/2day案例/js/index.js
@@ -16,7 +16,7 @@ function getboksList() {
   }).then(res => {
     // console.log(res);
     const bookList = res.data.data
-    console.log(bookList);
+    // console.log(bookList);
     const htmlStr = bookList.map((item, index) => {
       return `
           <tr>
@@ -24,7 +24,7 @@ function getboksList() {
           <td>${item.bookname}</td>
           <td>${item.author}</td>
           <td>${item.publisher}</td>
-          <td>
+          <td data-id=${item.id}>
             <span class="del">删除</span>
             <span class="edit">编辑</span>
           </td>
@@ -52,7 +52,7 @@ document.querySelector('.add-btn').addEventListener('click', () => {
   // 2.2 收集表单数据，并提交到服务器保存
   const addForm = document.querySelector('.add-form')
   const bookObj = serialize(addForm, { hash: true, empty: true })
-  console.log(bookObj);
+  // console.log(bookObj);
   axios({
     url: 'http://hmajax.itheima.net/api/books',
     method: 'post',
@@ -69,7 +69,51 @@ document.querySelector('.add-btn').addEventListener('click', () => {
     addModal.hide()
 
   })
+})
+/**
+ * 目标3：删除图书
+ *  3.1 删除元素绑定点击事件->获取图书id
+ *  3.2 调用删除接口
+ *  3.3 刷新图书列表
+ */
+// 3.1 删除元素 -》点击（事件委托）
+document.querySelector('.list').addEventListener('click', (e) => {
+  // console.log(e.target);
+  // 判断点击的是不是删除按钮
+  if (e.target.classList.contains('del')) {
+    // 获取图书ID（自定义属性）
+    const theId = e.target.parentNode.dataset.id
+    // console.log(theId);
+    // 3.2 调用删除接口
+    axios({
+      url: `http://hmajax.itheima.net/api/books/${theId}`,
+      method: 'DELETE',
+    }).then(() => {
+      getboksList()
+    })
+
+  }
 
 
+})
 
+
+/**
+ * 目标4：编辑图书
+ *  4.1 编辑弹框->显示和隐藏
+ *  4.2 获取当前编辑图书数据->回显到编辑表单中
+ *  4.3 提交保存修改，并刷新列表
+ */
+// 4.1 编辑弹框->显示和隐藏
+const ediDom = document.querySelector('.edit-modal')
+const eidModal = new bootstrap.Modal(ediDom)
+document.querySelector('.list').addEventListener('click', (e) => {
+  if (e.target.classList.contains('edit')) {
+    eidModal.show()
+
+  }
+})
+// 4.2 点击修改按钮-》隐藏弹框
+document.querySelector('.edit-btn').addEventListener('click', () => {
+  eidModal.hide()
 })
